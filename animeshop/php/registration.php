4f6002a3723f01ee5b4a03b08d3639dc179a0755
ololo322 <cpritula1@gmail.com>
2019-11-22 10:15:13 +0200

    oonime go
    Co-Authored-By: DimaSolonichenko <dimasolonichenko@users.noreply.github.com>

---
+++ animeshop/php/registration.php
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`username` varchar(100) NOT NULL,
+`firstname` varchar(100) NOT NULL,
+`lastname` varchar(100) NOT NULL,
+'date' int(11)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ animeshop/php/registrationDB.php
@@ -0,0 +1,8 @@
+<?php 
+$dbServerConnection = 'localhost';
+$dbUsername = "root";
+$dbPassword = "";
+$dbTittle = "RegistrationDB";
+$conn = mysqli_connect($dbServerConnection, $dbUsername, $dbPassword, $dbTittle);
+
+?>
